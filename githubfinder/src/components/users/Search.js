e7f35dba2287cfe3910576b148780ab9a4faa887
cguirgu <cguirgu@ncsu.edu>
2021-09-30 10:17:49 -0400

    Added alert and clear features

--- githubfinder/src/components/users/Search.js
+++ githubfinder/src/components/users/Search.js
@@ -8,17 +8,25 @@ export class Search extends Component {
 
 	static propTypes = {
 		searchUsers: PropTypes.func.isRequired,
+		clearUsers: PropTypes.func.isRequired,
+		showClear: PropTypes.bool.isRequired,
+		setAlert: PropTypes.func.isRequired,
 	}
 
 	onChange = (e) => this.setState({ [e.target.name]: e.target.value })
 
 	onSubmit = (e) => {
 		e.preventDefault()
-		this.props.searchUsers(this.state.text)
-		this.setState({ text: '' })
+		if (this.state.text === '') {
+			this.props.setAlert('Please enter something', 'light')
+		} else {
+			this.props.searchUsers(this.state.text)
+			this.setState({ text: '' })
+		}
 	}
 
 	render() {
+		const { showClear, clearUsers } = this.props
 		return (
 			<div>
 				<form onSubmit={this.onSubmit} className='form'>
@@ -31,6 +39,11 @@ export class Search extends Component {
 					/>
 					<input type='submit' className='btn btn-dark btn-block' />
 				</form>
+				{showClear && (
+					<button className='btn btn-light btn-block' onClick={clearUsers}>
+						Clear
+					</button>
+				)}
 			</div>
 		)
 	}
